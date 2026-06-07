ade3cb6aa2f451850820d87103b5cd8757ca6424
Shivan Hussain <shivanhussain6@gmail.com>
2026-06-07 16:10:57 +0530

    Update app.run host parameter
    Fix the host parameter in app.run to use '0.0.0.0/0'.

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def delete_task_form(task_id):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0/0", port=int(os.environ.get("PORT", 5000)))
