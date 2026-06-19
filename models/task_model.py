a68272211d554f162d2c6b12f8df2064f1bba4d5
Shivan_Hussain <shivanhussain6@gmail.com>
2026-06-19 16:50:53 +0530

    fix: sonarqube code coverage

--- models/task_model.py
+++ models/task_model.py
@@ -1,20 +1,25 @@
 from database import tasks_collection
 from bson import ObjectId
+from bson.errors import InvalidId
+
 
 def create_task(task_data: dict):
     """Insert a new task"""
     result = tasks_collection.insert_one(task_data)
     return str(result.inserted_id)
 
+
 def get_all_tasks():
     """Return all tasks"""
     return list(tasks_collection.find())
 
+
 def get_task_by_id(task_id):
     try:
         return tasks_collection.find_one({"_id": ObjectId(task_id)})
-    except:
+    except InvalidId:
         return None
+
 
 def update_task(task_id, update_data: dict):
     try:
@@ -23,12 +28,15 @@ def update_task(task_id, update_data: dict):
             {"$set": update_data}
         )
         return result.modified_count
-    except:
+    except InvalidId:
         return 0
+
 
 def delete_task(task_id):
     try:
-        result = tasks_collection.delete_one({"_id": ObjectId(task_id)})
+        result = tasks_collection.delete_one(
+            {"_id": ObjectId(task_id)}
+        )
         return result.deleted_count
-    except:
+    except InvalidId:
         return 0
